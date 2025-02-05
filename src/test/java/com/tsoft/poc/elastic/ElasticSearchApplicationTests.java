1dd47b1575face792e8d5991adfa5d205905e872
tarunnaik <tarun.naik@gmail.com>
2025-02-05 22:09:16 +0530

    Elastic Search POC created

---
+++ src/test/java/com/tsoft/poc/elastic/ElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsoft.poc.elastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
